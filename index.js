aa2b4a1c36dd65a2b4c8329f1ddf72601e03f781
Unknown <swilsondev@outlook.com>
2017-11-08 10:52:46 -0600

    Code is now generic.
    1) Added a system which creates express get handlers for all .html files in root.
    2) Added a 404 page, which is sent to any request for a bad file.

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 var express = require('express');
-var app = express();
+var server = express();
 var path = require("path");
+var fs = require("fs");
+var _ = require("lodash");
 
-app.get(["/","index.html"], function (req, res) {
+// --- Find all html files. ---
+var allFiles = fs.readdirSync("./");
+
+var htmlFiles = [];
+var htmlReg = /.html$/;
+_.forEach(allFiles, (value) => {
+    if (htmlReg.test(value) == true) {
+        htmlFiles.push("/" + value);
+    }
+});
+
+// --- Add express get handler for all files.
+_.forEach(htmlFiles, (fname) => {
+    server.get([fname, fname.substring(0,fname.length-5)], function (req, res) { //Array is used to allow omition of .html
+        res.sendFile(path.join(__dirname, fname));
+    });
+});
+
+//Add one for /
+server.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.get("/about.html", function (req, res) {
-    res.sendFile(path.join(__dirname, "/about.html"));
+//If there is no file, send 404
+server.get("*", function(req, res) {
+    res.status(404).sendFile(path.join(__dirname, "/notfound.html"));
 });
- 
-app.listen(3000, () => {
+
+// --- Launch server. --- 
+server.listen(3000, () => {
     console.log("Listening on port 3000.");
 });
